e795fe4ed2fce871e9e04674d2f816c66156d5e9
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2022-04-25 22:01:43 -0400

    Single Page Site with JS and CSS

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 theMeesh = {}
 
-theMeesh.copyEmail = function() {
+theMeesh.copyEmail = () => {
   let myEmail = document.querySelector('#myEmail').innerText;
   document.addEventListener('click', async function() {
     navigator.clipboard
@@ -8,8 +8,45 @@ theMeesh.copyEmail = function() {
   })
 }
 
+theMeesh.navTransition = () => {
+const homeNav = document.querySelector('#homeNav')
+const aboutNav = document.querySelector('#aboutNav')
+const portfolioNav = document.querySelector('#portfolioNav')
+const homePage = document.querySelector('#homePage')
+const aboutPage = document.querySelector('#aboutPage')
+const portfolioPage = document.querySelector('#portfolioPage')
+
+homeNav.addEventListener('click', function() {
+  homePage.classList.add('active');
+  aboutPage.classList.remove('active');
+  portfolioPage.classList.remove('active');
+  homePage.classList.remove('inactive');
+  aboutPage.classList.add('inactive');
+  portfolioPage.classList.add('inactive');
+})
+
+aboutNav.addEventListener('click', function() {
+  homePage.classList.remove('active');
+  aboutPage.classList.add('active');
+  portfolioPage.classList.remove('active');
+  homePage.classList.add('inactive');
+  aboutPage.classList.remove('inactive');
+  portfolioPage.classList.add('inactive');
+})
+
+portfolioNav.addEventListener('click', function() {
+  homePage.classList.remove('active');
+  aboutPage.classList.remove('active');
+  portfolioPage.classList.add('active');
+  homePage.classList.add('inactive');
+  aboutPage.classList.add('inactive');
+  portfolioPage.classList.remove('inactive');
+})
+}
+
 theMeesh.init = () => {
 theMeesh.copyEmail()
+theMeesh.navTransition()
 }
 
 theMeesh.init()
