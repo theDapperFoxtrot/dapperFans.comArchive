42e48cd657271b07c826ab4fae8ca23e6d8b47b3
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2022-07-07 17:05:36 -0400

    reverting

---
+++ script/script.js
@@ -0,0 +1,62 @@
+theMeesh = {}
+
+theMeesh.copyEmail = () => {
+  let myEmail = document.querySelector('#myEmail').innerText;
+  document.addEventListener('click', function() {
+    navigator.clipboard
+    .writeText('mishos.steve@gmail.com')
+  })
+}
+
+theMeesh.navTransition = () => {
+const homeNav = document.querySelector('#homeNav')
+const aboutNav = document.querySelector('#aboutNav')
+const portfolioNav = document.querySelector('#portfolioNav')
+const portfolioLink = document.querySelector('#portfolioLink')
+const homePage = document.querySelector('#homePage')
+const aboutPage = document.querySelector('#aboutPage')
+const portfolioPage = document.querySelector('#portfolioPage')
+
+homeNav.addEventListener('click', function() {
+  homePage.classList.add('active');
+  aboutPage.classList.remove('active');
+  portfolioPage.classList.remove('active');
+  homePage.classList.remove('inactive');
+  aboutPage.classList.add('inactive');
+  portfolioPage.classList.add('inactive');
+})
+
+aboutNav.addEventListener('click', function() {
+  homePage.classList.remove('active');
+  aboutPage.classList.add('active');
+  portfolioPage.classList.remove('active');
+  homePage.classList.add('inactive');
+  aboutPage.classList.remove('inactive');
+  portfolioPage.classList.add('inactive');
+})
+
+portfolioNav.addEventListener('click', function() {
+  homePage.classList.remove('active');
+  aboutPage.classList.remove('active');
+  portfolioPage.classList.add('active');
+  homePage.classList.add('inactive');
+  aboutPage.classList.add('inactive');
+  portfolioPage.classList.remove('inactive');
+})
+
+portfolioLink.addEventListener('click', function() {
+  homePage.classList.remove('active');
+  aboutPage.classList.remove('active');
+  portfolioPage.classList.add('active');
+  homePage.classList.add('inactive');
+  aboutPage.classList.add('inactive');
+  portfolioPage.classList.remove('inactive');
+})
+}
+
+theMeesh.init = () => {
+theMeesh.copyEmail()
+theMeesh.navTransition()
+}
+
+theMeesh.init()
